f44577feb4990e762f824577efb0c1565180f081
Vasco Pearson <Mar@MBPdeUtlizador4.Home>
2022-05-31 18:11:18 +0100

    .

--- src/test.c
+++ src/test.c
@@ -1,43 +1,24 @@
 #include "../inc/so_long.h"
 
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	render_next_frame(t_vars *strct)
 {
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	strct->mlx = mlx_init();
+	mlx_loop_hook(strct->mlx, render_next_frame, strct);
+	mlx_loop(strct->mlx);
+	return (0);
 }
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-	int		x;
-	int		y;
+	t_vars	*strct = NULL;
 
-	x = 50;
-	y = 50;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 920, 480, "Hello world!");
-	img.img = mlx_new_image(mlx, 920, 480);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	while(x > 5)
-	{
-		my_mlx_pixel_put(&img, x, y, 0x00FF0000);
-		x--;
-	}
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	strct->mlx = mlx_init();
+	mlx_loop_hook(strct->mlx, render_next_frame, strct);
+	mlx_loop(strct->mlx);
 }
